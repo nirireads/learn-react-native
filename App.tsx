78e3e6e97ee3a4423b1cabf133d5ad91f92502a1
nirsingh <nirireads@gmail.com>
2024-12-07 18:54:59 +0545

    Day2/finished-project

--- App.tsx
+++ App.tsx
@@ -1,22 +1,11 @@
-import {SafeAreaView, ScrollView} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
-import FlatCards from './components/FlatCards';
-import ElevatedCards from './components/ElevatedCards';
-import FancyCard from './components/FancyCard';
-import ActionCard from './components/ActionCard';
-import ContactList from './components/ContactList';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <FlatCards />
-        <ElevatedCards />
-        <FancyCard />
-        <ContactList />
-        <ActionCard />
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <Text>App</Text>
+    </View>
   );
 };
 
---
+++ App2.tsx
@@ -0,0 +1,23 @@
+import {SafeAreaView, ScrollView} from 'react-native';
+import React from 'react';
+import FlatCards from './components/App2/FlatCards';
+import ElevatedCards from './components/App2/ElevatedCards';
+import FancyCard from './components/App2/FancyCard';
+import ActionCard from './components/App2/ActionCard';
+import ContactList from './components/App2/ContactList';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+        <ElevatedCards />
+        <FancyCard />
+        <ContactList />
+        <ActionCard />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
