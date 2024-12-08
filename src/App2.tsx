c7ba5405cac4b30ef0b4a414193246cd6a0e1e35
nirsingh <nirireads@gmail.com>
2024-12-08 17:18:49 +0545

    day3/roll the dice

--- src/App2.tsx
+++ src/App2.tsx
@@ -1,10 +1,10 @@
 import {SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
-import FlatCards from './components/App2/FlatCards';
-import ElevatedCards from './components/App2/ElevatedCards';
-import FancyCard from './components/App2/FancyCard';
-import ActionCard from './components/App2/ActionCard';
-import ContactList from './components/App2/ContactList';
+import FlatCards from '../components/App2/FlatCards';
+import ElevatedCards from '../components/App2/ElevatedCards';
+import FancyCard from '../components/App2/FancyCard';
+import ActionCard from '../components/App2/ActionCard';
+import ContactList from '../components/App2/ContactList';
 
 const App = () => {
   return (
